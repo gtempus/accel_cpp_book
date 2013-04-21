11a10d023908e1598037da58423b7b7d9b9ed174
Gary Tempus <gtempus@codegaloot.com>
2013-04-21 02:42:26 +0000

    Final version of the chapter example.

--- looping_and_counting/greeting.cpp
+++ looping_and_counting/greeting.cpp
@@ -1,59 +1,58 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
 int main()
 {
   // ask for the person's name
-  std::cout << "Please enter your first name: ";
+  cout << "Please enter your first name: ";
 
   // read the name
-  std::string name;
-  std::cin >> name;
+  string name;
+  cin >> name;
 
   //build the message that we intend to write
-  const std::string greeting = "Hello, " + name + "!";
+  const string greeting = "Hello, " + name + "!";
 
   // the number of blanks surrounding the greeting
   const int pad = 1;
 
   //total number of rows to write
   const int rows = pad * 2 + 3;
-  const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+  const string::size_type cols = greeting.size() + pad * 2 + 2;
 
   // separate the output from the input
-  std::cout << std::endl;
+  cout << endl;
 
   //write rows rows of output
   int r = 0;
 
   // invariant: we have written r rows so far
-  while (r != rows) {
+  for(int r = 0; r != rows; ++r) {
     // write a row of output (as we will decribe in 2.4/22)
 
-    std::string::size_type c = 0;
+    string::size_type c = 0;
     
     // invariant: we have written c characters so far in the current row
     while (c != cols) {
-      if (r == 0 || r == rows - 1 || c == 0 || c == cols -1) {
-	std::cout << "*";
-	++c;
+      if (r == pad + 1 && c == pad + 1) {
+	cout << greeting;
+	c += greeting.size();
       } else {
-	// write one or more characters
-	// adjust the value of c to maintain the invariant
-	if (r == pad + 1 && c == pad + 1) {
-	  std::cout << greeting;
-	  c += greeting.size();
-	} else {
-	  std::cout << " ";
-	  ++c;
-	}
+	if (r == 0 || r == rows - 1 || c == 0 || c == cols -1)
+	  cout << "*";
+	else
+	  cout << " ";
+	++c;
       }
+      
     }
-
-    std::cout << std::endl;
-    ++r;
+    cout << endl;
   }
 
   return 0;
 }
-
