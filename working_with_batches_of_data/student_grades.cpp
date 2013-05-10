a069870892b7038376e5ca23920e589872d15204
Gary Tempus <gtempus@codegaloot.com>
2013-05-10 03:10:59 +0000

    Finsihed the chapter now on to the exercises.

--- working_with_batches_of_data/student_grades.cpp
+++ working_with_batches_of_data/student_grades.cpp
@@ -1,14 +1,18 @@
+#include <algorithm>
 #include <iomanip>
 #include <ios>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::setprecision;
+using std::sort;
 using std::streamsize;
 using std::string;
+using std::vector;
 
 int main()
 {
@@ -27,9 +31,9 @@ int main()
   cout << "Enter all your homework grades, "
     "followed by end-of-file: ";
 
-  // the number and sum of grades read so far
-  int count = 0;
-  double sum = 0;
+  // homework grades
+  vector<double> homework;
+  typedef vector<double>::size_type vec_sz;
 
   //a variable into which to read
   double x;
@@ -38,14 +42,31 @@ int main()
   //  we have read count grades so far, and
   //  sum is the sum of the first count grades
   while (cin >> x) {
-    ++count;
-    sum += x;
+    homework.push_back(x);
   }
+
+  vec_sz size = homework.size();
+
+  if (size == 0){
+    cout << endl << "You must enter your grades. "
+      "Please try again." << endl;
+    return 1;
+  }
+
+  sort(homework.begin(), homework.end());
+  vec_sz mid = size / 2;
+  double median;
+
+  // modulus operation is implementation specific so just check for one element in the vector.
+  if (size == 1)
+    median = homework[0];
+  else
+    median = mid % 2 == 0 ? (homework[mid] + homework[mid-1]) / 2 : homework[mid];
 
   //write the result
   streamsize prec = cout.precision();
   cout << "Your final grade is " << setprecision(3)
-       << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+       << 0.2 * midterm + 0.4 * final + 0.4 * median
        << setprecision(prec) << endl;
 
   return 0;
